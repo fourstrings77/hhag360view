93698ce69e3a8e815845b74cb0bfeb222dbb384a
Hauke Hagedorn <hagedorn.hauke@gmx.de>
2023-02-28 12:41:42 +0100

    add media extension

---
+++ src/DAL/Media/Aggregate/ThreesixtyDataEntity.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Hhag\ThreesixtyViewer\DAL\Media\Aggregate;
+
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityIdTrait;
+
+class ThreesixtyDataEntity extends Entity
+{
+    use EntityIdTrait;
+
+}
